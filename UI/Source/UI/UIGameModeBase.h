4a1478edc3607c58c8685a6c978e1bf95c8429d0
Letov_Maxim <mr.algree@gmail.com>
2022-05-27 16:49:46 +0300

    Init

---
+++ UI/Source/UI/UIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UI_API AUIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
